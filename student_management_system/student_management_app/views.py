fe0cf96ead62c77df36d030a4783a6447d2e5b05
gourav541 <85184131+gourav541@users.noreply.github.com>
2023-03-01 14:46:20 +0530

    2:46PM at office

--- student_management_system/student_management_app/views.py
+++ student_management_system/student_management_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
 
 # Create your views here.
 
 def showDemoPage(request):
     return render(request, 'demo.html')
+
+def ShowLoginPage(request):
+    return render(request,'login_page.html')
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        return HttpResponse(" Email: "+request.POST.get("email")+" Password "+request.POST.get("password"))
--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -130,9 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL= "student_management_app.CustomUser"
+AUTHENTICATION_BACKENDS=["student_management_app.Email"]
